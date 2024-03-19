09ef8255388e1acabe4ecffb49904a3fe4b83ce4
jacoblf314 <jacoblf314@gmail.com>
2024-03-19 18:35:57 -0400

    Revert "Merge branch 'main' of https://github.com/Dereck-Desjardins/Chevaleresk"
    This reverts commit ed9ac68d4a2a5f85d926d764a139ba661e49eb0a, reversing
    changes made to 88f5c6e036a2f53432b03caaeafa0beca16eb2d8.

--- boutique.php
+++ boutique.php
@@ -1,42 +1,8 @@
 <?php
-
-
-
-$content = '<div class="content"><div class="itemContainer">';
-//Ajouter les filtres et tri
-
-foreach($allItems as $oneItem){
-    $id = $oneItem->Id;
-    $nom = $oneItem->Nom;
-    $quantite = $oneItem->QuantiteStock; 
-    $typeItem = $oneItem->TypeItem;
-    $prix = $oneItem->Prix;
-    $photo = $oneItem->Photo;
-
-    if($quantite > 0){
-      $ItemHTML = <<<HTML
-        <div class="itemLayout">
-            <img src="$photo" alt="" class="photoItemShop">
-            <div class="shopItemMiddle">
-                <div class="nomItem">$nom</div>
-                <div class="typeItem">$typeItem</div>
-            </div>
-            <div class="shopItemRight">
-                <div class="prix">Prix: $prix </div>
-                <form action="">
-                    <input type="number" id="quantite" name="quantite" min="1" value="1">
-                    <input type="submit" value="Ajouter au panier">
-                </form>
-            </div>
-
+    $content = <<<HTML
+        <div class="content">
+            
         </div>
-      
-
-      HTML;
-    }
-
-}           
-
-     
-    $content = $content + '</div></div>';
+    HTML;
     include "views/master.php";
+?>
