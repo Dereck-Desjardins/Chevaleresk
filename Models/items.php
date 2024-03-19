10d0b726fa74c5003c7f70436a7bf45808f548d1
DereckD <dereck.desjardins@hotmail.com>
2024-03-19 13:39:25 -0400

    model objet

---
+++ Models/items.php
@@ -0,0 +1,41 @@
+<?php
+
+class Items 
+{
+    public $Id;
+    public $Nom;
+    public $QuantiteStock;
+    public $TypeItem;
+    public $Prix;
+    public $Photo;
+
+    public function __construct(){
+        $this->Id = 0;
+        $this->Nom = "";
+        $this->QuantiteStock = 0;
+        $this->TypeItem = "";
+        $this->Prix = 0;
+        $this->Photo = "";
+    }
+
+    public Function setNom($nom){
+        $this->Nom = $nom; 
+    }
+
+    public Function setQuantiteStock($quantiteStock){
+        $this->Nom = $quantiteStock; 
+    }
+    public Function setTypeItem($typeItem){
+        $this->Nom = $typeItem; 
+    }
+    public Function setPrix($prix){
+        $this->Nom = $prix; 
+    }
+    public Function setPhoto($photo){
+        $this->Nom = $photo; 
+    }
+}
+
+
+
+?>
