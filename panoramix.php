1896c4eae3cae517aeb361893608bf3f3b29227c
Dereck-Desjardins <dereck.desjardins@hotmail.com>
2024-04-11 15:29:49 -0400

    Panoramix Layout

--- panoramix.php
+++ panoramix.php
@@ -1,108 +1,61 @@
 <?php
-    $content = <<<HTML
-        <div class="content">
-            <div class="panoramixContainer">
-            <div class="panoramixMenu">
-                
-            </div>
-            <div class="allRecette">
-                <div class="recetteContainer">
-                    <div class="recetteLeft">
-                        <div class="recetteNom">Potion de vie</div>
+include_once "Models/joueurs.php";
+include_once 'Models/items.php';
+include_once 'MySql/db_connection.php';
+include_once 'php/sessionManager.php';
+
+$ImageFolder = "data/img/";
+
+$connecter = isset($_SESSION['currentPlayer']);
+
+$content = '<div class="content">   <div class="panoramixContainer"><div class="panoramixMenu"></div><div class="allRecette">';
+
+$potions = DB::getAllPotions();
+
+foreach ($potions as $potion){
+    $potionId = $potion->idItem;
+    $potionPhoto = $potion->photo;
+    $potionNom = $potion->nom;
+    $recettes = DB::getRecetteById($potionId);
+
+    $content .= <<<HTML
+        <div class="recetteContainer">
+            <div class="recetteLeft">
+            <div class="recetteNom">$potionNom</div>
+
+    HTML;
+
+    foreach($recettes as $recette){
+        $idElement = $recette->Elements_idItem;
+        $quantite = $recette->Quantite;
+
+        $element = DB::getItemById($idElement);
+        $elementPhoto = $element->photo;
+        $elementNom = $element->nom;
+        $recetteLeftHTML = <<<HTML
                         <div class="recetteIngredient ">
                             <div class="espaceImageIngredient">
-                                <img src="photo.jpg" alt="" class="imgIngredient">
+                                <img src="$ImageFolder$elementPhoto" alt="" class="imgIngredient">
                             </div>
-                            <div class="quantiteIngredient">X1</div>
+                            <div class="quantiteIngredient">X$quantite</div>
                         </div>
-                        <div class="recetteIngredient">
-                            <div class="espaceImageIngredient">
-                                <img src="photo.jpg" alt="" class="imgIngredient">
-                            </div>
-                            <div class="quantiteIngredient">X2</div>
-                        </div>
-                        <div class="recetteIngredient">
-                            <div class="espaceImageIngredient">
-                                <img src="photo.jpg" alt="" class="imgIngredient">
-                            </div>
-                            <div class="quantiteIngredient">X3</div>
-                        </div>
+        HTML;
+        $content .= $recetteLeftHTML;
+    }
+    $content .= <<<HTML
+            </div>
+                <div class="recetteRight">
+                    <div class="resultatRecette">
+                        <img src="$ImageFolder$potionPhoto" alt="" class="imgResultat">
                     </div>
-                    <div class="recetteRight">
-                        <div class="resultatRecette">
-                            <img src="photo.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
                 
-                <div class="recetteContainer">
-                    <div class="recetteLeft">
-                        <div class="recetteNom">Potion de vie</div>
-                        <div class="recetteIngredient ingredientNon">
-                            <div class="espaceImageIngredient">
-                                <img src="photo.jpg" alt="" class="imgIngredient">
-                            </div>
-                            <div class="quantiteIngredient">X1</div>
-                        </div>
-                        <div class="recetteIngredient ingredientOui">
-                            <div class="espaceImageIngredient">
-                                <img src="photo.jpg" alt="" class="imgIngredient">
-                            </div>
-                            <div class="quantiteIngredient">X2</div>
-                        </div>
-                        <div class="recetteIngredient ingredientPartiel">
-                            <div class="espaceImageIngredient ">
-                                <img src="photo.jpg" alt="" class="imgIngredient">
-                            </div>
-                            <div class="quantiteIngredient">X3</div>
-                        </div>
-                    </div>
-                    <div class="recetteRight">
-                        <div class="resultatRecette">
-                            <img src="photo.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="recetteContainer">
-                    <div class="recetteLeft">
-                        <div class="recetteNom">Potion de vie</div>
-                        <div class="recetteIngredient">
-                            <div class="espaceImageIngredient">
-                                <img src="photo.jpg" alt="" class="imgIngredient">
-                            </div>
-                            <div class="quantiteIngredient">X1</div>
-                        </div>
-                        <div class="recetteIngredient">
-                            <div class="espaceImageIngredient">
-                                <img src="photo.jpg" alt="" class="imgIngredient">
-                            </div>
-                            <div class="quantiteIngredient">X2</div>
-                        </div>
-                        <div class="recetteIngredient">
-                            <div class="espaceImageIngredient">
-                                <img src="photo.jpg" alt="" class="imgIngredient">
-                            </div>
-                            <div class="quantiteIngredient">X3</div>
-                        </div>
-                    </div>
-                    <div class="recetteRight">
-                        <div class="resultatRecette">
-                            <img src="photo.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>    
-            <div class="detailRecette">
-                <div class="recetteNomGrand">Potion de vie</div>
-                <div class="imageRecetteGrand">
-                    <img src="photo.jpg" alt="" class="imgResultatGrand">
-                </div>
-                <div class="btnCraft">
-                    <input type="submit" class="grandBtn">
-                </div>
             </div>
         </div>
-        </div>
     HTML;
-    include "views/master.php";
+}
+
+
+
+$content .= '</div></div></div>';
+
+include "views/master.php";
